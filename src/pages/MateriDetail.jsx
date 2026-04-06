3c64440c6a3c1fc0378f63342f35dfca36ad6251
Reva Nayla <revanano11@gmail.com>
2026-04-06 15:33:21 +0700

    materidetail

--- src/pages/MateriDetail.jsx
+++ src/pages/MateriDetail.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import api from "../api/axiosClient";
 import Swal from "sweetalert2";
+import styled from "styled-components";
 
 export default function MateriDetail() {
   const { id } = useParams();
