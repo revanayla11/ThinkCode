03d4a396e3b14a4da8b6b90f9e5e434f5f3fc36c
Reva Nayla <revanano11@gmail.com>
2026-04-19 13:25:08 +0700

    deploy hostinger

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_URL || "https://powderblue-squirrel-795269.hostingersite.com/api" || "http://localhost:5000/api"; // Default dengan /api untuk API calls
+const API_BASE = import.meta.env.VITE_API_URL || "https://powderblue-squirrel-795269.hostingersite.com/api"; // Default dengan /api untuk API calls
 const BASE_URL = API_BASE.replace('/api', '');
 
 function getHeaders(isForm = false) {
